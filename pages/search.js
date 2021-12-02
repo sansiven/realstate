083ea07781dbcee823c610646ba283b309c7cafb
sansiven <sansiven@gmail.com>
2021-12-03 00:42:13 +0545

    added key to a missing element in map

--- pages/search.js
+++ pages/search.js
@@ -36,7 +36,7 @@ const Search = ({properties}) => {
                 Properties {router.query.purpose}
             </Text>
             <Flex flexWrap="wrap">
-                {properties.map(property => <Property property={property} />)}
+                {properties.map(property => <Property property={property} key={property.id}/>)}
             </Flex>
             {properties.length === 0 && (
                 <Flex justifyContent="center" alignItems="center" flexDirection="column" marginTop="5" marginBottom="5">
